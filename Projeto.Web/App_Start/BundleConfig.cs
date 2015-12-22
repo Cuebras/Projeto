1bf7fe19a007ac7e911a6131b63a515c14881e40
cuebras@gmail.com <cuebras@gmail.com>
2015-12-22 17:04:36 -0600

    Base do site completa

--- Projeto.Web/App_Start/BundleConfig.cs
+++ Projeto.Web/App_Start/BundleConfig.cs
@@ -8,11 +8,23 @@ namespace Projeto.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/site.css"));
             bundles.Add(new ScriptBundle("~/bundles/ProjetoApp")
                        .IncludeDirectory("~/Scripts/Controllers", "*.js")
-                       .Include("~/Scripts/ProjetoApp.js"));
+                       .Include("~/Scripts/ProjetoApp.js", 
+                       "~/Scripts/Lib/angular.js",
+                       "~/Scripts/Lib/angular-resource.js",
+                       "~/Scripts/Lib/angular-route.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/Lib/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/Lib/bootstrap.js",
+                      "~/Scripts/Lib/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
 
             BundleTable.EnableOptimizations = true;
         }
--- Projeto.Web/Scripts/ProjetoApp.js
+++ Projeto.Web/Scripts/ProjetoApp.js
@@ -1,3 +1,13 @@
-﻿var ProjetoApp = angular.module('ProjetoApp', []);
+﻿var ProjetoApp = angular.module('ProjetoApp', [`ngRoute`]);
 
 ProjetoApp.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/About', {
+            templateUrl: '/Templates/About/about.html'
+        }).otherwise({ redirectTo: "/Index" });
+}
+configFunction.$inject = ['$routeProvider'];
+
+ProjetoApp.config(configFunction);
